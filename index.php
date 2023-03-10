e46357460f51106b4ee0d05c50e069acd8fa7cae
joetanx <90442032+joetanx@users.noreply.github.com>
2023-03-10 12:34:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -66,63 +66,50 @@
   <head>
     <link rel="icon" href="https://www.cyberark.com/wp-content/themes/understrap-child/favicon.ico">
     <title>CyberArk Demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
   <body>
-    <header>
-      <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container py-3">
+      <header>
+        <div class="d-flex align-items-center pb-3 mb-4 border-bottom">
+          <a href="https://www.cyberark.com" class="d-flex align-items-center">
+            <img src="https://www.cyberark.com/wp-content/uploads/2022/12/cyberark-logo-v2.svg" height="48">
+          </a>
+          <nav class="d-inline-flex ms-md-auto">
+            <a class="me-3 py-2 text-dark text-decoration-none" href="https://docs.cyberark.com">Docs</a>
+            <a class="me-3 py-2 text-dark text-decoration-none" href="https://cyberark-customers.force.com/mplace/s/">Marketplace</a>
+          </nav>
+        </div>
+        <div class="pricing-header p-3 pb-md-4 text-center">
+          <h1 class="display-6 fw-normal">CyberArk Integration Demo</h1>
+          <p class="fs-3 text-muted">Random World Cities!</p>
+        </div>
+      </header>
+      <main>
+        <h2 class="display-6 text-center pb-md-4">
+          <?php
+            echo '<b>'.$row['City'].'</b> is a city in <b>'.$row['District'].'</b>, <b>'.$row['Country'].'</b> with a population of <b>'.$row['Population'].'</b>';
+          ?>
+        </h2>
+        <div class="card p-4 col-lg-5 col-md-5 mx-auto text-center">
+          <p class="lead">
+            <?php
+              echo 'Connected to database <b>'.$data.'</b> on <b>'.$host.'</b>:<b>'.$port.'</b>';
+            ?>
+          </p>
+          <p class="lead">
+            <?php
+              echo 'Using username: <b>'.$user.'</b> and password: <b>'.$pass.'</b>';
+            ?>
+          </p>
+        </div>
+      </main>
+      <footer class="my-md-5 pt-md-5 border-top">
         <div class="container">
-          <img src="https://docs.cyberark.com/Product-Doc/OnlineHelp/Portal/Content/Resources/_TopNav/Images/Skin/lg-cyberark.svg">
+          <p class="float-end">A CyberArk demo by Joe Tan <a href="mailto:joe.tan@cyberark.com">✉</a></p>
+          <p class="mb-1">Style by <a href="https://getbootstrap.com/">Bootstrap</a>.</p>
         </div>
-      </div>
-    </header>
-    <main>
-      <section class="py-3 text-center container">
-        <div class="row py-lg-3">
-          <div class="col-lg-12 col-md-12 mx-auto">
-            <h1 class="fw-light">CyberArk Integration Demo</h1>
-            <p class="lead">&nbsp</p>
-            <h2 class="fw-light">Random World Cities!</h2>
-            <h3 class="fw-light">
-              <?php
-                echo '<b>'.$row['City'].'</b> is a city in <b>'.$row['District'].'</b>, <b>'.$row['Country'].'</b> with a population of <b>'.$row['Population'].'</b>';
-              ?>
-            </h3>
-            <p class="lead">&nbsp</p>
-            <div class="bg-light p-3 rounded col-lg-5 col-md-5 mx-auto">
-              <p class="lead">
-                <?php
-                  echo 'Host: <b>'.getenv('HOSTNAME').'</b>';
-                ?>
-              </p>
-              <p class="lead">
-                <?php
-                  echo 'Connected to database <b>'.$data.'</b> on <b>'.$host.'</b>:<b>'.$port.'</b>';
-                ?>
-              </p>
-              <p class="lead">
-                <?php
-                  echo 'Using username: <b>'.$user.'</b> and password: <b>'.$pass.'</b>';
-                ?>
-              </p>
-            </div>
-            <p class="lead">&nbsp</p>
-            <p>
-              <a href="https://docs.cyberark.com" class="btn btn-primary my-2">CyberArk Docs</a>
-              <a href="https://cyberark-customers.force.com/mplace/s/" class="btn btn-secondary my-2">CyberArk Marketplace</a>
-            </p>
-          </div>
-        </div>
-      </section>
-    </main>
-    <footer class="text-muted py-3">
-      <div class="container">
-        <p class="float-end mb-1">
-          <a href="#">Back to top</a>
-        </p>
-        <p class="mb-1">A CyberArk demo by Joe Tan <a href="mailto:joe.tan@cyberark.com">✉</a></p>
-        <p class="mb-0">Style by <a href="https://getbootstrap.com/">Bootstrap</a>.</p>
-      </div>
-    </footer>
+      </footer>
+    </div>
   </body>
 </html>
